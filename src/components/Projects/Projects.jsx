355a6835af599c5915b3473fef10e8051f6ec53f
cango91 <can.gologlu@gmail.com>
2023-09-22 16:34:41 -0400

    Logos update. Reorganize projects order

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,20 +4,20 @@ import './Projects.css';
 
 const projects = [
     {
-        title: 'Convault',
-        description: 'End-to-end encrypted, web-based chat-app, with a unique data-architecture for mitigating data leaks. Features multiple security layers and real-time messaging.',
-        technologies: ['React', 'MongoDB', 'Socket.io', 'Express', 'NodeJS', 'JavaScript', 'Heroku', 'CSS3'],
-        imgSrc: "screenshots/convault.webp",
-        githubLink: [{ title: "GitHub (frontend)", src: "https://github.com/cango91/convault-frontend" }, { title: "GitHub (backend)", src: "" }],
-        deploymentLink: { title: "Live Site", src: "https://github.com/cango91/convault-backend" }
-    },
-    {
         title: 'eyeConnect',
         description: 'A tele-medicine web-app for connecting medical professionals with specialists. Features encryption-at-rest and AI-assisted medical image classification.',
         technologies: ["MongoDB", "Express", "NodeJS", "JavaScript", "TensorFlow", "OpenCV", "HTML5", "CSS3", "Heroku", "Docker"],
         imgSrc: "screenshots/eyeConnect.webp",
         githubLink: [{ title: 'GitHub', src: "https://github.com/cango91/eye-connect" }],
         deploymentLink: { title: "Live Site", src: "https://eye-connect-6befc020749e.herokuapp.com/" }
+    },
+    {
+        title: 'Convault',
+        description: 'End-to-end encrypted, web-based chat-app, with a unique data-architecture for mitigating data leaks. Features multiple security layers and real-time messaging.',
+        technologies: ['React', 'MongoDB', 'Socket.io', 'Express', 'NodeJS', 'JavaScript', 'Heroku', 'CSS3'],
+        imgSrc: "screenshots/convault.webp",
+        githubLink: [{ title: "GitHub (frontend)", src: "https://github.com/cango91/convault-frontend" }, { title: "GitHub (backend)", src: "" }],
+        deploymentLink: { title: "Live Site", src: "https://github.com/cango91/convault-backend" }
     },
     {
         title: 'Café Normalé',
